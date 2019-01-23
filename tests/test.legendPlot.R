3720353911589512ba64eeeb269c46e1f23d4c19
Jason Sinnwell <sinnwell.jason@mayo.edu>
2019-01-23 08:33:50 -0600

    legend example update

--- tests/test.legendPlot.R
+++ tests/test.legendPlot.R
@@ -12,10 +12,15 @@ legendPlot(ped1,  affected.label=c("cancer","available"))
 legendPlot(ped1, col=ped1$affected[,2]+1,  affected.label=c("cancer","available"))
 legendPlot(ped1, col=ped1$affected[,2]+1, col.label=c("no dna", "dna"), affected.label=c("cancer","available"))
 ## more informative example with 2 affected status, consent-to-study, and vital status
+
+## availability could be something like "consented", or having DNA available, so use consent from the availability status
 consented <- ped1$affected[,2]
+
 affected1 <- cbind(ped1$affected[,1], (ped1$id %in% c(108, 104, 131, 132, 140)))
-aff.label1 <- c("cancer","TII-Diab")
-vital1 <- 1 + ped1$id %in% c(105:108, 135:138, 115, 132)
-## some things not working...
+aff.label1 <- c("Cancer","TII-Diab")
+
+## add vital status 0=alive, 1=died  (puts a slash through the plot symbol
+ped1$status <- ped1$id %in% c(105:108, 135:138, 115, 132)
+
 legendPlot(ped1, col=consented+1, col.label=c("no consent","consent"), affected=affected1, status=vital1, affected.label=aff.label1)
 
