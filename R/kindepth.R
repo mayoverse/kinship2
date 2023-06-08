5bdf816f67b18fafc5bbd227fe352a2c85f0b447
LouisBzh <louislenezet@gmail.com>
2023-06-08 11:45:10 +0200

    Format indentation

--- R/kindepth.R
+++ R/kindepth.R
@@ -103,22 +103,22 @@ kindepth <- function(id, dad_id, mom_id, align = FALSE) {
   ## It may be possible to do better alignment when the pedigree has loops,
   ## but it is definitely beyond this program, perhaps in autohint one day.
 
-    chaseup <- function(x, midx, didx) {
-      new <- c(midx[x], didx[x]) # mother and father
+  chaseup <- function(x, midx, didx) {
+    new <- c(midx[x], didx[x]) # mother and father
+    new <- new[new > 0]
+    while (length(new) > 1) {
+      x <- unique(c(x, new))
+      new <- c(midx[new], didx[new])
       new <- new[new > 0]
-      while (length(new) > 1) {
-        x <- unique(c(x, new))
-        new <- c(midx[new], didx[new])
-        new <- new[new > 0]
-      }
-      x
-    } ## chaseup()
+    }
+    x
+  } ## chaseup()
 
-    ## First deal with any parents who are founders
-    ##  They all start with depth 0
-    dads <- didx[midx > 0 & didx > 0] # the father side of all spouse pairs
-    moms <- midx[midx > 0 & didx > 0]
-    if (0) {
+  ## First deal with any parents who are founders
+  ##  They all start with depth 0
+  dads <- didx[midx > 0 & didx > 0] # the father side of all spouse pairs
+  moms <- midx[midx > 0 & didx > 0]
+  if (0) {
     founder <- (midx == 0 & didx == 0)
     if (any(founder[dads])) {
       drow <- which(founder[dads]) # which pairs
@@ -137,78 +137,78 @@ kindepth <- function(id, dad_id, mom_id, align = FALSE) {
   }
   ## Get rid of duplicate pairs, which occur for any spouse with
   ##  multiple offspring
-    dups <- duplicated(dads + moms * n)
-    if (any(dups)) {
-      dads <- dads[!dups]
-      moms <- moms[!dups]
+  dups <- duplicated(dads + moms * n)
+  if (any(dups)) {
+    dads <- dads[!dups]
+    moms <- moms[!dups]
+  }
+
+  npair <- length(dads)
+  done <- rep(FALSE, npair) # couples that are taken care of
+  while (TRUE) {
+    pairs.to.fix <- which((depth[dads] != depth[moms]) & !done)
+    if (length(pairs.to.fix) == 0) {
+      break
     }
+    temp <- pmax(depth[dads], depth[moms])[pairs.to.fix]
+    who <- min(pairs.to.fix[temp == min(temp)]) # the chosen couple
 
-    npair <- length(dads)
-    done <- rep(FALSE, npair) # couples that are taken care of
-    while (TRUE) {
-      pairs.to.fix <- which((depth[dads] != depth[moms]) & !done)
-      if (length(pairs.to.fix) == 0) {
-        break
+    good <- moms[who]
+    bad <- dads[who]
+    if (depth[dads[who]] > depth[moms[who]]) {
+      good <- dads[who]
+      bad <- moms[who]
+    }
+    abad <- chaseup(bad, midx, didx)
+    if (length(abad) == 1 && sum(c(dads, moms) == bad) == 1) {
+      # simple case, a solitary marry-in
+      depth[bad] <- depth[good]
+    } else {
+      agood <- chaseup(good, midx, didx) # ancestors of the "good" side
+      ## For spouse chasing, I need to exclude the given pair
+      tdad <- dads[-who]
+      tmom <- moms[-who]
+      while (1) {
+        ## spouses of any on agood list
+        spouse <- c(
+          tmom[!is.na(match(tdad, agood))],
+          tdad[!is.na(match(tmom, agood))]
+        )
+        temp <- unique(c(agood, spouse))
+        temp <- unique(chaseup(temp, midx, didx)) # parents
+        kids <- (!is.na(match(midx, temp)) | !is.na(match(didx, temp)))
+        temp <- unique(c(temp, (1:n)[kids & depth <= depth[good]]))
+        if (length(temp) == length(agood)) {
+          break
+        } else {
+          agood <- temp
+        }
       }
-      temp <- pmax(depth[dads], depth[moms])[pairs.to.fix]
-      who <- min(pairs.to.fix[temp == min(temp)]) # the chosen couple
 
-      good <- moms[who]
-      bad <- dads[who]
-      if (depth[dads[who]] > depth[moms[who]]) {
-        good <- dads[who]
-        bad <- moms[who]
-      }
-      abad <- chaseup(bad, midx, didx)
-      if (length(abad) == 1 && sum(c(dads, moms) == bad) == 1) {
-        # simple case, a solitary marry-in
-        depth[bad] <- depth[good]
-      } else {
-        agood <- chaseup(good, midx, didx) # ancestors of the "good" side
-        ## For spouse chasing, I need to exclude the given pair
-        tdad <- dads[-who]
-        tmom <- moms[-who]
-        while (1) {
-          ## spouses of any on agood list
-          spouse <- c(
-            tmom[!is.na(match(tdad, agood))],
-            tdad[!is.na(match(tmom, agood))]
-          )
-          temp <- unique(c(agood, spouse))
-          temp <- unique(chaseup(temp, midx, didx)) # parents
-          kids <- (!is.na(match(midx, temp)) | !is.na(match(didx, temp)))
-          temp <- unique(c(temp, (1:n)[kids & depth <= depth[good]]))
-          if (length(temp) == length(agood)) {
+      if (all(match(abad, agood, nomatch = 0) == 0)) {
+      ## shift it down
+      depth[abad] <- depth[abad] + (depth[good] - depth[bad])
+
+      ## Siblings may have had children: make sure all kids are
+      ##   below their parents.  It's easiest to run through the
+      ##   whole tree
+      for (i in 0:n) {
+          parents <- which(depth == i)
+          child <- match(midx, parents, nomatch = 0) + 
+            match(didx, parents, nomatch = 0)
+          if (all(child == 0)) {
             break
-          } else {
-            agood <- temp
           }
-        }
-
-        if (all(match(abad, agood, nomatch = 0) == 0)) {
-        ## shift it down
-        depth[abad] <- depth[abad] + (depth[good] - depth[bad])
-
-        ## Siblings may have had children: make sure all kids are
-        ##   below their parents.  It's easiest to run through the
-        ##   whole tree
-        for (i in 0:n) {
-            parents <- which(depth == i)
-            child <- match(midx, parents, nomatch = 0) + 
-              match(didx, parents, nomatch = 0)
-            if (all(child == 0)) {
-              break
-            }
-            depth[child > 0] <- pmax(i + 1, depth[child > 0])
-          }
+          depth[child > 0] <- pmax(i + 1, depth[child > 0])
         }
       }
-      ## Once a subject has been shifted, we don't allow them to instigate
-      ## yet another shift, possibly on another level
-      done[dads == bad | moms == bad] <- TRUE
-    } ## while(TRUE)
-    if (all(depth > 0)) {
-      stop("You found a bug in kindepth's alignment code!")
     }
-    return(depth)
+    ## Once a subject has been shifted, we don't allow them to instigate
+    ## yet another shift, possibly on another level
+    done[dads == bad | moms == bad] <- TRUE
+  } ## while(TRUE)
+  if (all(depth > 0)) {
+    stop("You found a bug in kindepth's alignment code!")
+  }
+  return(depth)
 }
