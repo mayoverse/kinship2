a09e7d5db6d52c9c8b8c76a003cb0660c86a24e8
Louis <louislenezet@gmail.com>
2023-06-10 00:02:35 +0200

    Add expectation to num child test

--- tests/testthat/test-num_child.R
+++ tests/testthat/test-num_child.R
@@ -25,4 +25,7 @@ test_that("Num child",{
     code = c(4, 4, 1)
   )
   df <- num_child(df)
+  expect_equal(df$num_child_dir, c(5, 4, 4, 4, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0))
+  expect_equal(df$num_child_tot, c(6, 5, 4, 4, 0, 0, 2, 0, 6, 0, 0, 0, 0, 0))
+  expect_equal(df$num_child_ind, c(1, 1, 0, 0, 0, 0, 1, 0, 4, 0, 0, 0, 0, 0))
 })
