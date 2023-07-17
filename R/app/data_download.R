418d3ffb95f1f492224c8c99f043c645b6a5b4f4
LouisBzh <louislenezet@gmail.com>
2023-07-17 12:38:27 +0200

    Improve UI data downloading

--- R/app/data_download.R
+++ R/app/data_download.R
@@ -4,37 +4,44 @@ data_download_ui <- function(id) {
     ns <- shiny::NS(id)
     shiny::tagList(
         shiny::uiOutput(ns("data_text")),
-        shiny::uiOutput(ns("download_data"))
+        shiny::uiOutput(ns("btn_dwld"))
     )
 }
 
-data_download_server <- function(id, df, label) {
+data_download_server <- function(id, df, filename, label = NULL, helper = TRUE) {
     stopifnot(shiny::is.reactive(df))
     ns <- shiny::NS(id)
     shiny::moduleServer(id, function(input, output, session) {
+        output$data_dwld <- shiny::downloadHandler(filename = function() {
+                paste(filename, ".csv", sep = "")
+            },
+                content = function(file) {
+                write.csv2(df(), file)
+            })
         shiny::observeEvent(df(), {
             if (nrow(df()) == 0) {
                 output$data_text <- shiny::renderUI({
                     shiny::HTML(paste(label, "doesn't have any rows"))
                 })
-                output$download_data <- shiny::renderUI({
+                output$btn_dwld <- shiny::renderUI({
                     NULL
                 })
             } else {
-                output$data_text <- shiny::renderUI({
-                    shiny::HTML(paste(label, "with", nrow(df()), "rows",
-                    "<br/>", "(Data can only be export to Download folder)"))
-                })
-                output$download_data <- shiny::renderUI({
-                    output$data_downloader <- shiny::downloadHandler(filename = function() {
-                        paste(label, Sys.Date(), ".csv", sep = "")
-                    },
-                        content = function(file) {
-                        write.csv2(df(), file)
+                if (helper) {
+                    output$data_text <- shiny::renderUI({
+                        shiny::HTML(paste(label, "with", nrow(df()), "rows",
+                        "<br/>", "(Data can only be export to Download folder)"))
                     })
-                    shiny::downloadButton(ns("data_downloader"),
-                        paste("Download", label, "as .csv"))
+                } else {
+                    output$data_text <- shiny::renderUI({
+                        NULL
+                    })
+                }
+
+                output$btn_dwld <- shiny::renderUI({
+                    shiny::downloadButton(ns("data_dwld"), label = label)
                 })
+
             }
         })
     })
@@ -47,7 +54,9 @@ data_download_demo <- function() {
     server <- function(input, output, session) {
         data_download_server("datafile",
             shiny::reactive({mtcars}),
-            "mtcars")
+            "mtcars_data_file", "mtcars")
     }
     shiny::shinyApp(ui, server)
 }
+
+data_download_demo()
