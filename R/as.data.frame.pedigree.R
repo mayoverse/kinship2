0c295f7c3d8bfe7a8f577ee5ce321ba17b31d4e6
LouisBzh <louislenezet@gmail.com>
2023-07-26 16:55:53 +0200

    Delete PedigreeListSimplify as data frame

--- R/as.data.frame.pedigree.R
+++ R/as.data.frame.pedigree.R
@@ -18,18 +18,6 @@
 #' @seealso `pedigree`
 #' @export
 as.data.frame.pedigree <- function(x, ...) {
-    dadid <- momid <- rep(0, length(x$id))
-    dadid[x$findex > 0] <- x$id[x$findex]
-    momid[x$mindex > 0] <- x$id[x$mindex]
-    df <- data.frame(id = x$id, dadid = dadid, momid = momid, sex = x$sex)
-
-    if (!is.null(x$affected)) {
-        df$affected <- x$affected
-    }
-
-    if (!is.null(x$status)) {
-        df$status <- x$status
-    }
-    return(df)
+    return(x$ped)
 }
 TRUE
