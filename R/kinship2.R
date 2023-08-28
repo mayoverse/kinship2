f1b594ed6d36ae03cf0109e11727863135050bbf
LouisBzh <louislenezet@gmail.com>
2023-08-28 16:12:54 +0200

    Add kinship description

--- R/kinship2.R
+++ R/kinship2.R
@@ -8,13 +8,14 @@
 #' \\url{https://cran.r-project.org/package=kinship2}
 #'
 #' @section Functions:
-#' Below are listed some of the most widely used functions available in \\code{arsenal}:
+#' Below are listed some of the most widely used functions available
+#' in \\code{arsenal}:
 #'
 #' \\code{\\link{pedigree}}: Contstructor of the pedigree class,
 #' given identifiers, sex, affection status(es), and special relationships
 #'
-#' \\code{\\link{kinship}}: Calculates the kinship matrix, the probability having
-#' an allele sampled from two individuals be the same via IBD.
+#' \\code{\\link{kinship}}: Calculates the kinship matrix, the probability
+#' having an allele sampled from two individuals be the same via IBD.
 #'
 #' \\code{\\link{plot.pedigree}}: Plot method for a pedigree object.
 #' Allows extra information to be included in the id under the plot symbol
@@ -30,7 +31,8 @@
 #'
 #' @section Data:
 #' \\code{\\link{sample.ped}}: Pedigree example data sets with two pedigrees
-#' \\code{\\link{minnbreast}}: Larger cohort of pedigrees from MN breast cancer study
+#' \\code{\\link{minnbreast}}: Larger cohort of pedigrees from MN breast cancer
+#' study
 #'
 #' @examples
 #' library(kinship2)
