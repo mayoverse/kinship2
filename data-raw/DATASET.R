663b8e5246a2943a95118fec21dbbc7b48181014
LouisBzh <louislenezet@gmail.com>
2023-08-31 16:27:10 +0200

    Set sampleped ids as character

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,4 +6,8 @@
 sampleped <- read.delim("data-raw/sampleped.tab",
     header = TRUE, sep = " ", stringsAsFactors = FALSE
 )
+sampleped[c("family", "id", "dadid", "momid")] <- as.data.frame(
+    lapply(sampleped[c("family", "id", "dadid", "momid")], as.character)
+)
+summary(sampleped)
 usethis::use_data(sampleped, overwrite = TRUE)
