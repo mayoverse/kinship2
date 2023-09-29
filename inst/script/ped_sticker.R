f443261ff3d83109a5d90dbd7adca98dbe0ab2e6
LouisBzh <louislenezet@gmail.com>
2023-09-29 22:04:08 +0200

    Update sticker

--- inst/script/ped_sticker.R
+++ inst/script/ped_sticker.R
@@ -9,7 +9,7 @@ library(Pedigree)
 data(sampleped)
 
 # Create pedigree object
-ped1 <- pedigree(sampleped[sampleped$family == "1", ])
+ped1 <- pedigree(sampleped[sampleped$family == "1", -1])
 
 # Shrink pedigree
 ped1trim <- shrink(ped1, max_bits = 12)
@@ -20,7 +20,7 @@ png(output_dir,
     width = 500, height = 300, units = "px", bg = "#FFFFFF00")
 op <- par(mar = rep(0.5, 4))
 
-plot(ped1trim$pedObj, mar = rep(0, 4))
+plot(ped1trim$pedObj, mar = rep(0, 4), aff_mark = FALSE, symbolsize = 1.5)
 legend(
     x = 3.5, y = 2.7, legend = c("DNA Available", "UnAvailable"),
     pch = c(1, 1), col = c(2, 1), bty = "n", cex = 1
@@ -30,11 +30,11 @@ dev.off()
 par(op)
 
 # Create sticker
-s <- sticker(
+s <- hexSticker::sticker(
     output_dir,
-    package = "Pedigree", p_size = 60, p_color = "#000000",
+    package = "Pedigree", p_size = 55, p_color = "#000000",
     p_x = 1, p_y = 1.5,
-    s_x = 1, s_y = 0.8, s_width = 0.8, s_height = 0.5,
+    s_x = 1, s_y = 0.8, s_width = 0.7, s_height = 0.4,
     h_fill = "#FFFFFF00", h_color = "#3576ac",
     filename = "inst/figures/icon_Pedigree.png", dpi = 800
 )
